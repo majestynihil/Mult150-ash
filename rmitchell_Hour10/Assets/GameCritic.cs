40adbf82318d6a3d6aad99f0aa46814fe435af93
majestynihil <majestynihil@gmail.com>
2025-05-04 21:36:54 -0700

    Chaos Ball!!!!

---
+++ rmitchell_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,9 @@
+/* I thought the game was good happy medium, the chaos balls and collisions added some difficulty but it was overall fun. */
+
+/* Slowing the balls down  would make it easier, and 
+ * adding obsticles such as other blocks that the balls could bounce off of would make it more difficult */
+
+/* Giving the goals a little sound when they are destoryed like when you win big at the casion, would be excitng */
+
+/* I liked the interactions of the different balls it was very choatic and had me feeling like a kid playing a silly game.
+ * Getting the balls to the goals I thought were a tedious task since all the balls are knocking into each other and possibly the camera view */
